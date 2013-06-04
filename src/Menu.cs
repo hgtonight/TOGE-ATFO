2b9095a8c5e0c2e8af62101d2a843f32a7f31590
Michael Kautzman <mkautzm@gmail.com>
2013-06-03 20:52:42 -0500

    added MenuItem

---
+++ src/Menu.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace TOGE_ATFO
+{
+    class MenuItem
+    {
+        SpriteFont menuFont = Content.Load<SpriteFont>("Calibri");
+
+        String text;
+        int value;
+        Rectangle location;
+        Color color;
+
+        public Rectangle Location
+        {
+            get { return location; }
+        }
+
+        public MenuItem(String text, int value, Rectangle location, Color color)
+        {
+            this.text = text;
+            this.value = value;
+            this.location = location;
+            this.color = color;
+        }
+        public void Draw(SpriteBatch item)
+        {
+            item.DrawString(menuFont, text, new Vector2(location), color);
+        }
+    }
+}
---
+++ src/MenuItem.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+
+namespace TOGE_ATFO
+{
+    class MenuItem
+    {
+        SpriteFont menuFont; //Not used yet -- to be handled by Content Manager
+
+        String text;
+        int value;
+        Vector2 location;
+        Color color;
+
+        public Vector2 Location
+        {
+            get { return location; }
+        }
+
+        public MenuItem(String text, int value, Vector2 location, Color color)
+        {
+            this.text = text;
+            this.value = value;
+            this.location = location;
+            this.color = color;
+        }
+        public void Draw(SpriteBatch item)
+        {
+            item.DrawString(menuFont, text, location, color);
+        }
+    }
+}
